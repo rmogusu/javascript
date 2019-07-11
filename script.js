e504f5ee3e47ab3d2ad7ce396416eb05a0c89e2d
mrsfreddy <rmogusu123@gmal.com>
2019-07-11 04:03:28 -0400

    added javascrit.js file

---
+++ script.js
@@ -0,0 +1,5 @@
+var userName=prompt("enter your Name");
+var userAge=prompt("enter your Age");
+var userFood=prompt("enter your favoriteFood");
+var sentence=("My name is " +userName + " i am "+ userAge +" years old " + "my favorite food is "+userFood);
+alert(sentence);
